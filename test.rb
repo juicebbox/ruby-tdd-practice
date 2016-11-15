19d7e6af94febc79028a3bdfc615b834c6f10cde
Ivan Stankov <juicebbox@gmail.com>
2016-11-15 15:07:58 +0200

    first commit

---
+++ test.rb
@@ -0,0 +1,5 @@
+# This is a practice TDD project
+# First I have absolutely no idea of what would i create.
+# I do this for two main reasons:
+# - practice what I have read so far about TDD and ruby
+# - have fun
